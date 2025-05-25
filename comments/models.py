ee4d5230652d9df478ff6bedbf756986925a7889
Abdelrahman mohamed <abdhsrag280@gmail.com>
2025-05-25 08:22:07 +0300

    commet the forgin keys to test

--- comments/models.py
+++ comments/models.py
@@ -5,8 +5,8 @@ class Comments(models.Model):
     ID = models.AutoField(primary_key=True)
     content = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
-    user_id = models.ForeignKey('user.User', on_delete=models.CASCADE, null=True, blank=True) #remove null & blank after merge
-    project_id = models.ForeignKey('project.Project', on_delete=models.CASCADE, null=True, blank=True) #remove null & blank after merge
+    # user_id = models.ForeignKey('user.User', on_delete=models.CASCADE, null=True, blank=True) #remove null & blank after merge
+    # project_id = models.ForeignKey('project.Project', on_delete=models.CASCADE, null=True, blank=True) #remove null & blank after merge
 
     def __str__(self):
         return f"{self.content},{self.date}"
