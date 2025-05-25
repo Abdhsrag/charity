0881bbc6c051080101db08acf4e176886b8189ec
Abdelrahman mohamed <abdhsrag280@gmail.com>
2025-05-25 09:39:44 +0300

    created urls & added views func & tested

--- comments/api/views.py
+++ comments/api/views.py
@@ -1,7 +1,43 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+# from rest_framework.permissions import IsAuthenticated
 from comments.models import Comments
 from .serializers import CommentSerializer
 
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comments.objects.all()
     serializer_class = CommentSerializer
+    # permission_classes = [IsAuthenticated]
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(
+            {"message": "All good", "data": serializer.data},
+            status=status.HTTP_201_CREATED,
+            headers=headers
+        )
+
+    def retrieve(self, request, *args, **kwargs):
+        try:
+            instance = self.get_object()
+            serializer = self.get_serializer(instance)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Exception:
+            return Response(
+                {"detail": "Not found."},
+                status=status.HTTP_404_NOT_FOUND
+            )
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        return Response(
+            {"message": "Comment updated", "data": serializer.data},
+            status=status.HTTP_200_OK
+        )
