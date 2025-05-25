e8eadcfe2c42fde144b3e9828ecaef2bcc1f95bc
Abdelrahman mohamed <abdhsrag280@gmail.com>
2025-05-25 07:11:31 +0300

    created comments model

--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Comments(models.Model):
+    ID = models.AutoField(primary_key=True)
+    content = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    user_id = models.ForeignKey('user.User', on_delete=models.CASCADE)
+    project_id = models.ForeignKey('project.Project', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.content},{self.date}"
