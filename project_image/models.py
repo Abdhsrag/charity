ecb64cd875cb890f5bcaa9097126a23b26143fb4
Abdelrahman mohamed <abdhsrag280@gmail.com>
2025-05-25 07:30:09 +0300

    added project_image model and settings

---
+++ project_image/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Project_image(models.Model):
+    ID = models.AutoField(primary_key=True)
+    url = models.TextField()
+    project_id = models.ForeignKey('project.Project', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.url}"
