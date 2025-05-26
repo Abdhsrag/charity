4586b35b27d7363bca3312a36b8ce25b0484f508
habiba <habibaahmed30>
2025-05-26 12:54:34 +0200

    model project done

--- charity/settings.py
+++ charity/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'project.apps.ProjectConfig',
-    'tag.apps.TagConfig',
+    'rate.apps.TagConfig',
 ]
 
 MIDDLEWARE = [
--- tag/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Tag(models.Model):
-    id=models.IntegerField(primary_key=True)
-    name=models.CharField(max_length=100)
-
-    def __str__(self):
-        return f"{self.name}-{self.id}"
