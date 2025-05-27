f17bb66c78388a5b1944c11e542d0423c290ea0a
Abdelrahman mohamed <abdhsrag280@gmail.com>
2025-05-27 19:23:14 +0300

    added latest_projects to project api view & added to the api text file

--- project/api/views.py
+++ project/api/views.py
@@ -1,6 +1,9 @@
 from rest_framework import viewsets
 from ..models import Project
 from .serializers import ProjectSerializer
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import status
 
 
 
@@ -8,3 +11,12 @@ from .serializers import ProjectSerializer
 class ProjectViewSet(viewsets.ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
+
+    @action(detail=False, methods=['get'], url_path='projects_latest')
+    def latest_projects(self, request):
+        latest_projects = Project.objects.order_by('-S_time')[:5]
+        serializer = self.get_serializer(latest_projects, many=True)
+        return Response(
+            {"message": "Latest 5 projects retrieved successfully", "data": serializer.data},
+            status=status.HTTP_200_OK
+        )
