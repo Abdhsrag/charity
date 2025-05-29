f40ccce1f89f9dc530e61a29eb9a92160fcdbbda
Abdelrahman mohamed <abdhsrag280@gmail.com>
2025-05-29 19:37:26 +0300

    changed serlizer for project_image & comments

--- project_image/models.py
+++ project_image/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Project_image(models.Model):
-    ID = models.AutoField(primary_key=True)
+    id = models.AutoField(primary_key=True)
     url = models.ImageField(upload_to='project_images/media')
     project_id = models.ForeignKey('project.Project', on_delete=models.CASCADE)
 
